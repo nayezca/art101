b944e033441685899ca638705a69e2020fc9c356
nayezca <nguzmanj@ucsc.edu>
2024-11-10 20:42:57 -0800

    LAB 9
    Exploring jQuery

--- lab1/js/lab.js
+++ lab1/js/lab.js
@@ -6,16 +6,11 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+});
