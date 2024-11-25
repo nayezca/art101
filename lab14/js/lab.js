3215ef155c4472a18132e142426771a99143ad5d
nayezca <nguzmanj@ucsc.edu>
2024-11-25 13:25:21 -0800

    LAB 14
    index.html & linked to homepage

---
+++ lab14/js/lab.js
@@ -0,0 +1,9 @@
+/*
+   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Nayezca Guzman-Jubb
+   Date: 2024
+*/
+
